018116156ed29368e1eefa963e1928384e3688b2
punyak <61309142+Puny-hue@users.noreply.github.com>
2020-09-06 21:59:02 +0530

    Add files via upload

---
+++ ListSlicing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 17:08:23 2020
+
+@author: PUNYA
+"""
+
+l =[10,20,30,40,50,60,70]
+print(l[2:4])
+
+print(l[4])
+
+
+print(l[::-1])  # reverse
+
+print(l[-4])
